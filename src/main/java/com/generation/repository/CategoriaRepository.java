7eec7326b12ce7c9b1d3e325074865a037444a1d
unknown <contatogabrezende@gmail.com>
2024-06-10 13:38:37 -0300

    Primeira versão do crud categoria

---
+++ src/main/java/com/generation/repository/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.generation.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.model.CategoriaModel;
+
+
+public interface CategoriaRepository extends JpaRepository<CategoriaModel, Long>{
+
+}
+
